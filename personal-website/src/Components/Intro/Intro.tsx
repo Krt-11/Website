1c93082469addeaf3dbb16cfd26aa087041235a3
Krt-11 <krutinrs@gmail.com>
2025-02-28 11:13:10 -0500

    feat: add new content to the intro page

--- personal-website/src/Components/Intro/Intro.tsx
+++ personal-website/src/Components/Intro/Intro.tsx
@@ -1,20 +1,42 @@
 import { Box, Typography } from "@mui/material";
-import Typical from "react-typical";
+// import Typical from "react-typical";
 import "./Intro.css";
 
 function Intro() {
   return (
-    <Box className="typing-container">
-      <Box className="typing-background" />
+    <Box className="intro-box">
+      <Box className="typing">
+        <Typography
+          variant="h4"
+          sx={{ fontFamily: "monospace", fontWeight: "600" }}
+        >
+          Hi, I'm Krutin
+        </Typography>
+      </Box>
+      <Box className="bullet-points">
+        <Typography variant="h6">
+          <ul className="bullet-list">
+            <li>I'm a software engineer</li>
+            <li>I do full stack development</li>
+            <li>I also do mobile development</li>
+            <li>Check out some of my work</li>
+          </ul>
+        </Typography>
+      </Box>
 
-      <Typography component="h2" className="typing-text">
-        <Typical
-          steps={["Hi I'm Krutin", 1500, "", 1000]}
-          loop={Infinity}
-          wrapper="span"
-        />
-        <Box className="typing-cursor" />
-      </Typography>
+      {/*       
+      <Box className="typing-container">
+        <Box className="typing-background" />
+
+        <Typography component="h2" className="typing-text">
+          <Typical
+            steps={["Hi I'm Krutin", 1500, "", 1000]}
+            loop={Infinity}
+            wrapper="span"
+          />
+        </Typography>
+        <Box className="bullet-points"></Box>
+      </Box> */}
     </Box>
   );
 }
