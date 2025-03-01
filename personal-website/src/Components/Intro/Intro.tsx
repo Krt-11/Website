1591fe2210b33ed02b378fb0fa86f048e2689a7e
Krt-11 <krutinrs@gmail.com>
2025-02-28 19:32:55 -0500

    feat: add animation for arrow that shows up on clicking the checkout out my work

--- personal-website/src/Components/Intro/Intro.tsx
+++ personal-website/src/Components/Intro/Intro.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import { Box, Typography } from "@mui/material";
-import "./Intro.css";
 import { smoothScrollTo } from "../../SharedFunctions/ScrollAnimation";
+import "./Intro.css";
 
 function Intro() {
+  const [showArrow, setShowArrow] = useState(false);
+
   const handleScrollToAbout = () => {
-    const aboutSection = document.getElementById("about");
-    if (aboutSection) {
-      const yOffset = -64;
-      smoothScrollTo(aboutSection.offsetTop + yOffset);
-    }
+    setShowArrow(true);
+
+    setTimeout(() => {
+      const aboutSection = document.getElementById("about");
+      if (aboutSection) {
+        const yOffset = -80;
+        const targetY =
+          aboutSection.getBoundingClientRect().top + window.scrollY + yOffset;
+        smoothScrollTo(targetY);
+      }
+
+      setTimeout(() => {
+        setShowArrow(false);
+      }, 600);
+    }, 1200);
   };
 
   return (
@@ -16,29 +29,27 @@ function Intro() {
       <Box className="typing">
         <Typography
           variant="h4"
-          sx={{
-            fontFamily: "monospace",
-            fontWeight: "600",
-            position: "relative",
-            zIndex: "1",
-          }}
+          sx={{ fontFamily: "monospace", fontWeight: "600" }}
         >
           Hi, I'm Krutin
         </Typography>
         <Box className="typingColorBox" />
       </Box>
+
       <Box className="bullet-points">
         <Typography variant="h6">
           <ul className="bullet-list">
             <li>I'm a software engineer</li>
             <li>I do full stack development</li>
             <li>I also do mobile development</li>
-            <li className="clickable" onClick={handleScrollToAbout}>
+            <li className="work-link" onClick={handleScrollToAbout}>
               Check out some of my work
             </li>
           </ul>
         </Typography>
       </Box>
+
+      {showArrow && <div className="scroll-arrow">↓</div>}
     </Box>
   );
 }
